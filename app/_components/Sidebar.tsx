f5a403ba2db34dc8bce5e8d1d931ca9b89285d3c
Charles Dauda <charlesdauda676@gmail.com>
2026-07-30 16:05:14 +0000

    initial commit

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,14 +1,27 @@
 'use client'
 
-import { BarChart3, LayoutDashboard, LogOut, Package, Settings, User, Users } from "lucide-react"
+import { BarChart3, LayoutDashboard, LogOut, Package, Settings, User,  Users, X } from "lucide-react"
+
 interface SidebarProps {
     isSidebarOpen: boolean;
+    setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Sidebar = ({ isSidebarOpen }: SidebarProps) => {
+const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }: SidebarProps) => {
 
     return (
-        <aside className={`flex flex-col ${isSidebarOpen ? "w-64" : "w-20"} lg:w-64 min-h-screen border-r border-gray-300 p-5 flex-shrink-0 transition-all duration-300 `} >
+        <aside className={`fixed top-0 left-0 z-50 h-screen bg-white border-r border-gray-300 p-5 w-64 flex flex-col transition-transform duration-300
+                ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} lg:static lg:translate-x-0 `} >
+
+            {/* Close Button Mobile */}
+            <div className="flex justify-end lg:hidden mb-4">
+                <button
+                    onClick={() => setIsSidebarOpen(false)}
+                    className="p-2 rounded-md hover:bg-gray-100" >
+                    <X size={22} />
+                </button>
+            </div>
+
             <div className="mb-10">
                 <h1 className="text-2xl font-extrabold text-blue-950 mt-4 hidden lg:block">
                     Admin Dashboard
@@ -39,7 +52,7 @@ const Sidebar = ({ isSidebarOpen }: SidebarProps) => {
                         Products
                     </span>
                 </a>
-                
+
                 <a className="flex items-center gap-2 p-3 rounded-sm hover:bg-gray-100 cursor-pointer text-gray-700">
                     <BarChart3 size={20}/>
                     <span className={`${isSidebarOpen ? "block" : "hidden"} lg:block`}>
@@ -63,7 +76,7 @@ const Sidebar = ({ isSidebarOpen }: SidebarProps) => {
                         Profile
                     </span>
                 </a>
-                
+
                 <a className="flex items-center gap-3 p-3 rounded-sm hover:bg-gray-100 cursor-pointer text-gray-700">
                     <LogOut size={20}/>
                     <span className={`${isSidebarOpen ? "block" : "hidden"} lg:block`}>
@@ -72,9 +85,7 @@ const Sidebar = ({ isSidebarOpen }: SidebarProps) => {
                 </a>
             </div>
         </aside>
-
     )
 }
-
 
 export default Sidebar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,23 +19,25 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="flex min-h-screen">
-
-          <Sidebar isSidebarOpen={isSidebarOpen} />
-
+         <Sidebar
+              isSidebarOpen={isSidebarOpen}
+              setIsSidebarOpen={setIsSidebarOpen}
+            />
+            {isSidebarOpen && (
+              <div
+              className="fixed inset-0 bg-black/40 z-40 lg:hidden"
+              onClick={() => setIsSidebarOpen(false)}
+              />
+)}
           <div className="flex-1 flex flex-col">
-
             <Navbar 
               setIsSidebarOpen={setIsSidebarOpen}
             />
-
             <main className="flex-1 overflow-auto">
               {children}
             </main>
-
             <Footer />
-
           </div>
-
         </div>
       </body>
     </html>
